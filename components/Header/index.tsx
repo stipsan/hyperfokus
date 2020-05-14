be252812100025c7b7144ca3f0c5212c29098c2e
Cody Olsen <cody.olsen@finn.no>
2020-05-14 19:40:55 +0200

    test some more

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -10,7 +10,9 @@ export default ({ title }: Props) => {
   return (
     <header className={styles.navbar}>
       <div className={cx(styles.left, 'justify-start')}>Left</div>
-      <div className={styles.center}>{title ?? 'HyperFokus'}</div>
+      <div className={cx(styles.center, 'h-inherit')}>
+        {title ?? 'HyperFokus'}
+      </div>
       <div className={cx(styles.right, 'justify-end')}>
         Right <br />
         Right

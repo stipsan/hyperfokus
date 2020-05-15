794bfe1f5350256595f09d4f4b6bd2a4e03a6150
Cody Olsen <cody.olsen@finn.no>
2020-05-15 21:34:26 +0200

    Test fade in trick

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,33 @@
 import { Provider as AuthProvider } from 'components/Auth'
 import HeadTitle from 'components/HeadTitle'
 import type { AppProps } from 'next/app'
-import { Suspense } from 'react'
+import { Suspense, useEffect } from 'react'
 import { RecoilRoot } from 'recoil'
 
 import 'styles/_app.css'
 
 // @TODO install @types/recoil when available
 
-export default ({ Component, pageProps }: AppProps) => (
-  <Suspense fallback={'Loading...'}>
-    <HeadTitle />
-    <RecoilRoot>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </RecoilRoot>
-  </Suspense>
-)
+export default ({ Component, pageProps }: AppProps) => {
+  // Suspense fallbacks are invisible for a set delay to avoid spinner flash on first load
+  // and adding this class ensures it's turned off in case something else is suspended after
+  // the app is fully loaded and the user navigates somewhere
+  useEffect(() => document.body.classList.add('loaded'), [])
+
+  return (
+    <Suspense
+      fallback={
+        <main className="flex h-screen items-center justify-center text-gray-700 text-xl w-screen loading">
+          Loading...
+        </main>
+      }
+    >
+      <HeadTitle />
+      <RecoilRoot>
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </RecoilRoot>
+    </Suspense>
+  )
+}

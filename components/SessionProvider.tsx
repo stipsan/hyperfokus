b4de117f0ae2ef650c378b9311c911e8053a85a4
Cody Olsen <cody.olsen@finn.no>
2020-05-15 22:01:23 +0200

    Test SSR error

---
+++ components/SessionProvider.tsx
@@ -0,0 +1,55 @@
+import { createContext, useState, useContext } from 'react'
+import type { Dispatch, SetStateAction } from 'react'
+
+type SessionState = '' | 'localstorage' | 'firebase'
+
+const SessionValueContext = createContext<SessionState>(undefined)
+const SessionSetStateContext = createContext<
+  Dispatch<SetStateAction<SessionState>>
+>(undefined)
+
+export const Provider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [session, setSession] = useState<SessionState>(
+    typeof window === 'undefined'
+      ? ''
+      : () => {
+          const unsafeState = localStorage.getItem(
+            'hyperfokus.storage'
+          ) as SessionState
+          switch (unsafeState) {
+            case 'localstorage':
+            case 'firebase':
+              return unsafeState
+            default:
+              return ''
+          }
+        }
+  )
+
+  return (
+    <SessionSetStateContext.Provider value={setSession}>
+      <SessionValueContext.Provider value={session}>
+        {children}
+      </SessionValueContext.Provider>
+    </SessionSetStateContext.Provider>
+  )
+}
+
+export const useSession = () => {
+  // Suspend on the server only as we're reading the provider state from localStorage
+  // which is a sync operation, thus we don't need to suspend on it in the client
+  if (typeof window === 'undefined') {
+    //throw new Promise((resolve) => setTimeout(() => resolve()))
+  }
+  const session = useContext(SessionValueContext)
+
+  // It's critical that this hook is never used outside a Provider, or it'll definitely cause bugs.
+  // The state setter ensures that the value is only ever null if the provider is missing.
+  if (session === undefined) {
+    throw new TypeError(`useSession requires an SessionProvider wrapper`)
+  }
+  console.log(session)
+  return session
+}
--- pages/help.tsx
+++ pages/help.tsx
@@ -2,18 +2,21 @@ import { useAuthDispatch, useAuthState } from 'components/Auth'
 import Header from 'components/Header'
 import HeadTitle from 'components/HeadTitle'
 import Link from 'next/link'
+import { useSession } from 'components/SessionProvider'
 
 const title = 'Help'
 
 export default () => {
   const authState = useAuthState()
   const authDispatch = useAuthDispatch()
+  const session = useSession()
 
   return (
     <>
       <HeadTitle>{title}</HeadTitle>
       <Header title={title} />
       <main className="min-h-50vh flex items-center justify-center">
+        Session: {session}
         <a
           className="rounded-lg px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 bg-gray-500 hover:bg-gray-600 md:text-lg xl:text-base text-white font-semibold leading-tight shadow-md"
           href="mailto:stipsan@gmail.com"

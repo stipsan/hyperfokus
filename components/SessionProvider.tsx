d5ee050ba5f52b554f65bded68281f7d5a9cadbd
Cody Olsen <cody.olsen@finn.no>
2020-05-15 22:09:28 +0200

    enable suspense on hook

--- components/SessionProvider.tsx
+++ components/SessionProvider.tsx
@@ -41,8 +41,9 @@ export const useSession = () => {
   // Suspend on the server only as we're reading the provider state from localStorage
   // which is a sync operation, thus we don't need to suspend on it in the client
   if (typeof window === 'undefined') {
-    //throw new Promise((resolve) => setTimeout(() => resolve()))
+    throw new Promise((resolve) => setTimeout(() => resolve()))
   }
+
   const session = useContext(SessionValueContext)
 
   // It's critical that this hook is never used outside a Provider, or it'll definitely cause bugs.
@@ -50,6 +51,6 @@ export const useSession = () => {
   if (session === undefined) {
     throw new TypeError(`useSession requires an SessionProvider wrapper`)
   }
-  console.log(session)
+
   return session
 }

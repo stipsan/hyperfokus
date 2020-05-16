fa4be5c378fa750dc8c243c6ba7099ac2eeb9d9b
Cody Olsen <cody.olsen@finn.no>
2020-05-17 00:01:00 +0200

    make it act like a logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,7 @@
+import styles from './Logo.module.css'
+
 export default () => (
-  <span role="img" aria-label="HyperFokus logo">
+  <span className={styles.logo} role="img" aria-label="HyperFokus logo">
     <span className="font-bold text-black">Hyper</span>
     <span className="rounded inline-block bg-black text-white ml-1 px-1 font-bold">
       Fokus

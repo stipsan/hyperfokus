e04af68f67afea39788f22d215f5ab5bde5a2ddf
Cody Olsen <cody.olsen@finn.no>
2020-05-18 20:02:12 +0200

    No more sticky thanks to safari
    Will revisit later

--- components/DialogToolbar.tsx
+++ components/DialogToolbar.tsx
@@ -3,13 +3,15 @@ import cx from 'classnames'
 type Props = {
   left?: React.ReactNode
   right: React.ReactNode
+  sticky?: boolean
 }
 
-export default ({ left, right }: Props) => (
+export default ({ left, right, sticky }: Props) => (
   <div
     className={cx(
       //styles.safarifix,
-      'flex items-center justify-between bg-white bottom-0 sm:sticky py-3 sm:py-6'
+      { 'bottom-0 sm:sticky bg-white': sticky },
+      'flex items-center justify-between py-3 sm:py-6'
     )}
   >
     <div>{left}</div>

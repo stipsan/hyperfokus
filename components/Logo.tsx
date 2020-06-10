d3f8f3f434f7a41ab34ae31ea56ed34af3146611
Cody Olsen <cody.olsen@finn.no>
2020-06-10 13:19:32 +0200

    fix: link the logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import styles from './Logo.module.css'
 
 export default () => (
-  <span className={styles.logo} role="img" aria-label="HyperFokus logo">
-    <span className="font-extrabold text-blue-800">Hyper</span>
-    <span className="rounded inline-block bg-blue-700 text-white ml-1 px-1 font-bold">
-      Fokus
-    </span>
-  </span>
+  <Link href="/">
+    <a className={styles.logo} role="img" aria-label="HyperFokus logo">
+      <span className="font-extrabold text-blue-800">Hyper</span>
+      <span className="rounded inline-block bg-blue-700 text-white ml-1 px-1 font-bold">
+        Fokus
+      </span>
+    </a>
+  </Link>
 )

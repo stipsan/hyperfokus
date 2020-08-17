b81d2a250c13a39f9c324129f88da02beb58a749
Cody Olsen <cody.olsen@finn.no>
2020-08-17 19:09:40 +0200

    enable webpack 5

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-const withTM = require('next-transpile-modules')([
-  '@react-spring/core',
-  '@react-spring/web',
-])
+const withTM = require('next-transpile-modules')(
+  ['@react-spring/core', '@react-spring/web'],
+  { unstable_webpack5: true }
+)
 
 module.exports = withTM({
   devIndicators: {

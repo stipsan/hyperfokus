db50c8af3fa987c1e3861b29a10f708c9f4a3fba
Cody Olsen <stipsan@gmail.com>
2021-05-27 00:22:56 +0200

    bumpy bump

--- hooks/session.ts
+++ hooks/session.ts
@@ -27,7 +27,7 @@ export const useSessionValue = () => {
   // Suspend on the server only as we're reading the provider initial state from localStorage
   // which is a sync operation, thus we don't need to suspend on it in the client
   if (typeof window === 'undefined') {
-    throw new Promise((resolve) => setTimeout(() => resolve()))
+    throw new Promise((resolve) => setTimeout(() => resolve(void 0)))
   }
 
   const session = useRecoilValue(sessionProviderState)
